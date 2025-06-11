f0290bb48d751868755895e6a57d1ae04d02f8cf
Nikhil2000-git <burranikhil2000@gmail.com>
2025-06-11 22:04:57 +0530

    frontend of foodie

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Foodie is your go-to food delivery platform that brings your favorite meals from local restaurants straight to your doorstep. Whether you're craving spicy biryani, cheesy pizza, or healthy salads, we’ve got you covered — anytime, anywhere.</p>
+         <button>View Menu</button>
+      
+      
+      </div>
+
+
+        
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/components/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,58 @@
+import React, { useContext } from "react";
+import "./PlaceOrder.css";
+import { StoreContext } from "../../../context/StoreContex";
+
+const PlaceOrder = () => {
+  const { getTotalCartAmount } = useContext(StoreContext);
+  return (
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <input type="email" placeholder="Email Address" />
+        <input type="text" placeholder="Street" />
+        <div className="multi-fields">
+          <input type="text" placeholder="City " />
+          <input type="text" placeholder="State" />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder="Pincode " />
+          <input type="text" placeholder="Country " />
+        </div>
+        <input type="text" placeholder="Mobile Number" />
+      </div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Sub Total</p>
+              <p>₹{getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery fee</p>
+              <p>₹{getTotalCartAmount() === 0 ? "0" : 45}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>
+                ₹
+                {getTotalCartAmount() + getTotalCartAmount() === 0
+                  ? "0"
+                  : getTotalCartAmount() + 45}
+              </b>
+            </div>
+          </div>
+          <button>PROCEED TO PAYMENT</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default PlaceOrder;
